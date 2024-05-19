e0dfb4b5c4d8926159b3f01ce286c87673ae8aea
Nurudeen Ahmed <ahmednurudeen06@gmail.com>
2024-05-19 14:01:49 +0100

    fix: Router routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,20 +10,7 @@ router.post('/signup', authController.signup);
 // Protect all routes after this middleware
 router.use(authController.protect);
 
-router.delete('/deleteMe', userController.deleteMe);
-
 // Only admin have permission to access for the below APIs 
 router.use(authController.restrictTo('admin'));
-
-router
-    .route('/')
-    .get(userController.getAllUsers);
-
-
-router
-    .route('/:id')
-    .get(userController.getUser)
-    .patch(userController.updateUser)
-    .delete(userController.deleteUser);
 
 module.exports = router;
