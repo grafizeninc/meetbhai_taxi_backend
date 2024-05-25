26f24a2e30ba9bcf01cb9888091fa778b0244762
Nurudeen Ahmed <ahmednurudeen06@gmail.com>
2024-05-25 10:36:44 +0100

    refractor: Remove Rate Limiter

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const limiter = rateLimit({
     windowMs: 60 * 60 * 1000,
     message: 'Too Many Request from this IP, please try again in an hour'
 });
-app.use('/api', limiter);
+// app.use('/api', limiter);
 
 app.use(express.json({
     limit: '15kb'
