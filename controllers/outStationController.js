28699649ed21a9bbd4524033d84698578f2e6573
mansi godhani <godhanimansi71@gmail.com>
2024-06-06 02:11:29 +0530

    Minor update for out station api

--- controllers/outStationController.js
+++ controllers/outStationController.js
@@ -1,5 +1,7 @@
 const base = require('./baseController');
 const OutStation = require("../models/outStationModel");
+const State = require("../models/stateModel");
+const AppError = require("../utils/appError");
 
 exports.getAll = base.getAll(OutStation);
 exports.getOne = base.getOne(OutStation);
@@ -7,14 +9,31 @@ exports.update = base.updateOne(OutStation);
 exports.delete = base.deleteOne(OutStation);
 exports.add = async (req, res, next) => {
     try {
-        const OutStation = await OutStation.create(req.body)
+        const state = await State.findById(req.body.stateId);
+        if (!state) {
+            return next(
+                new AppError(401, "fail", "No State found with that id"),
+                req,
+                res,
+                next
+            );
+        }
+
+        let result;
+        const alreadyExist = await OutStation.findOne({ stateId: req.body.stateId });
+
+        if (alreadyExist) {
+            result = await OutStation.findByIdAndUpdate(alreadyExist._id, req.body, { new: true, upsert: true, lean: true });
+        } else {
+            result = await OutStation.create(req.body);
+        }
         
-          res.status(201).json({
+        res.status(201).json({
             status: "success",
             data: {
-                OutStation,
+                result,
             },
-          });
+        });
     } catch (err) {
         next(err);
     }
--- routes/outStationRoutes.js
+++ routes/outStationRoutes.js
@@ -6,16 +6,16 @@ const outStationController = require('./../controllers/outStationController');
 // Protect all routes after this middleware
 router.use(authController.protect);
 
-router.get('/', outStationController.getAll);
+router.get('/out-stations', outStationController.getAll);
 router.get('/out-station/:id', outStationController.getOne);
 
 
 // Only admin have permission to access for the below APIs 
 router.use(authController.restrictTo('admin'));
 
-router.post('/out-station/:id', outStationController.update);
+router.put('/out-station/edit/:id', outStationController.update);
 router.post('/out-station/add', outStationController.add);
-router.delete('/out-station/:id', outStationController.delete);
+router.delete('/out-station/delete/:id', outStationController.delete);
 
 
 module.exports = router;
